1ea24b59424e2c9da1b6a4173edd2ce26bb8b210
Mauricio Maletta <mauricio_maletta@hotmail.com>
2020-06-03 09:02:50 -0300

    alterar e excluir usuário com jdbc postgres

--- src/main/java/br/com/fundamentosjsp/dao/UsuarioDAO.java
+++ src/main/java/br/com/fundamentosjsp/dao/UsuarioDAO.java
@@ -31,8 +31,45 @@ public class UsuarioDAO {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public void alterar(Usuario usu) {
+		Connection con  = ConnectionFactory.getConnection();
+		String sql = "update usuarios set nome=?, login=?, senha=?  where id = ?"; 
 		
+		// try automaticamente fecha o PreparedStatement que implementa a classe AutoCloseable
+		try(PreparedStatement preparador = con.prepareStatement(sql)) {
+			
+			preparador.setString(1, usu.getNome());
+			preparador.setString(2, usu.getLogin());
+			preparador.setString(3, usu.getSenha());
+			preparador.setInt(4, usu.getId());
+			preparador.executeUpdate();
+			
+			System.out.println("Alterado com sucesso");
+			con.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
+	
+	public void excluir(Usuario usu) {
+		Connection con  = ConnectionFactory.getConnection();
+		String sql = "delete from usuarios where id = ?"; 
+		
+		// try automaticamente fecha o PreparedStatement que implementa a classe AutoCloseable
+		try(PreparedStatement preparador = con.prepareStatement(sql)) {
+			
+			preparador.setInt(1, usu.getId());
+			preparador.executeUpdate();
+			
+			System.out.println("Excluído com sucesso");
+			con.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
--- src/test/java/br/com/fundamentosjsp/TestUsuarioDAO.java
+++ src/test/java/br/com/fundamentosjsp/TestUsuarioDAO.java
@@ -1,20 +1,50 @@
 package br.com.fundamentosjsp;
+
 import br.com.fundamentosjsp.model.*;
 import br.com.fundamentosjsp.dao.*;
+
 public class TestUsuarioDAO {
 
 	public static void main(String[] args) {
-		
-		//Criando usuario
+		// testCadastrar("jão", "jão", "123");
+		// testAlterar(2,"mano", "mano", "123");
+		testExcluir(2);
+	}
+
+	public static void testCadastrar(String nome, String login, String senha) {
+		// Criando usuario
 		Usuario usu = new Usuario();
-		usu.setNome("Maletta");
-		usu.setLogin("maletta");
-		usu.setSenha("123");
-		
+		usu.setNome(nome);
+		usu.setLogin(login);
+		usu.setSenha(senha);
+
 		// Cadastrando
 		UsuarioDAO usuDAO = new UsuarioDAO();
 		usuDAO.cadastrar(usu);
-
 	}
+	
+	public static void testAlterar(Integer id, String nome, String login, String senha) {
+		// Criando usuario
+		Usuario usu = new Usuario();
+		usu.setId(id);
+		usu.setNome(nome);
+		usu.setLogin(login);
+		usu.setSenha(senha);
+
+		// Alterando
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.alterar(usu);
+	}
+	
+	public static void testExcluir(Integer id) {
+		// Criando usuario
+		Usuario usu = new Usuario();
+		usu.setId(id);
+
+		// Excluindo
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.excluir(usu);
+	}
+
 
 }
